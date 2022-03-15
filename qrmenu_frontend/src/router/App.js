3bb10f2c234682462fd8c8822671fdf0d0a889c7
Ariel Pinto <arielp@goja.com>
2022-03-15 18:18:32 -0400

    Install react-toastify and configure in app.js and index.js

--- qrmenu_frontend/src/router/App.js
+++ qrmenu_frontend/src/router/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {ToastContainer} from "react-toastify";
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   )
 }
 
