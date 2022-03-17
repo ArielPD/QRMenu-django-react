ca4c2377115f30374e37eb0db35ea676d834c89a
Ariel Pinto <arielp@goja.com>
2022-03-17 16:52:21 -0400

    Create a model for Places, create serializers and view in backend

--- qrmenu_backend/qrmenu_backend/urls.py
+++ qrmenu_backend/qrmenu_backend/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+
+    path('api/places/', views.PlaceList.as_view(), name='place-list'),
 ]
